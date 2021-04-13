7a216ff13e94ae2331eda5217c5f1b47f552d3d3
Ahmed Mera <ahmedmera81136@gmail.com>
2021-04-13 13:10:13 +0200

    init commit

---
+++ src/main/java/com/example/javadb/JavaDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.javadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDbApplication.class, args);
+    }
+
+}
